b0192dd604a56d71f8ada5cdc8d97ad3d9c8b1c8
Shahla Almasri <sh@almasris.com>
2018-03-05 19:55:22 -0500

    version 2

---
+++ GamOn/AppDelegate 2.swift
@@ -0,0 +1,98 @@
+//
+//  AppDelegate.swift
+//  GamOn
+//
+//  Created by Shahla Almasri Hafez on 2/18/18.
+//
+
+import UIKit
+import Contacts
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    static var contacts = [CNContact]()
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "RecentPlayers")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
+                
+                print("Unresolved error, \((error as NSError).userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    ///
+    /// Saves the list of recent players to the database
+    ///
+    func saveRecentPlayers(recentList: inout [CNContact]) {
+        // Make sure the list does not grow beyound 10 entries
+        if recentList.count > 10 {
+            recentList.remove(at: 10)
+        }
+        
+        let context = persistentContainer.viewContext
+        let entity = NSEntityDescription.insertNewObject(forEntityName: "Players", into: context)
+        entity.setValue(recentList, forKey: "recentPlayers")
+        do {
+            try context.save()
+        } catch {
+            print("Failed saving recent players")
+        }
+    }
+    
+    ///
+    /// Loads the list of recent players from the database
+    ///
+    func loadRecentPlayers() -> [CNContact] {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Players")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try persistentContainer.viewContext.fetch(request)
+            if let result = result as? [NSManagedObject], result.count == 1 {
+                return result[0].value(forKey: "recentPlayers") as! [CNContact]
+            } else {
+                print("No recent players")
+            }
+        } catch {
+            print("Failed loading recent players")
+        }
+        
+        return []
+    }
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
